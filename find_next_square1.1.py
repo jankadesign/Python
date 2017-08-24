f6d75d4dd868159a47dfcc3f33a906ab1a3a59e8
JanUnPython <jankadesign@gmail.com>
2017-08-24 23:19:00 +0300

    file delete

--- find_next_square1.1.py
+++
@@ -1,9 +0,0 @@
-
-def find_next_square(sq):
-    # Return the next square if sq is a square, -1 otherwise
-    root = sq**0.5
-    if root.is_integer():
-        return (root+1)**2
-    else: return -1
-
-print find_next_square(15)
