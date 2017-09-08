6915b6bc1375442d4a13b5612e48d1a1f5acc4ab
JanUnPython <jankadesign@gmail.com>
2017-09-08 12:30:40 +0300

    refactoring

--- my_Fib.py
+++ my_Fib.py
@@ -1,8 +1,8 @@
 def fib(n):
- a = [0,1]
- for i in range (2,n): 
-  a.append(a[i-2]+a[i-1])
- print a
+    a = [0,1]
+    for i in range (2,n):
+        a.append(a[i-2]+a[i-1])
+    print a
 
 def fib1(n):
     a, b = 0, 1
@@ -11,8 +11,12 @@ def fib1(n):
         print b
 
 def fib2(n):
- a = [0,1]
- a.append(a[i-2]+a[i-1] for i in range(2,n))
- print a
+    s = (2,3,6,1,8)
+    #a = (n for n in range(0, n + 1))
+    for i in s :
+        print i
 
-fib2(10)
+def fib3(n) :
+    print [a[0] + a[1] for a in [[b, b+1] for b in range(n)]]
+
+fib3(10)
